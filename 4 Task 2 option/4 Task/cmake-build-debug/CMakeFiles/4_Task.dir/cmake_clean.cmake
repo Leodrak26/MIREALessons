6de14e104257e9f9057efd0afbd45a50391a7f73
Leonid <42905852+Leodrak26@users.noreply.github.com>
2021-04-07 19:32:58 +0300

    Add files via upload

---
+++ 4 Task 2 option/4 Task/cmake-build-debug/CMakeFiles/4_Task.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "4_Task"
+  "4_Task.pdb"
+  "CMakeFiles/4_Task.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/4_Task.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
